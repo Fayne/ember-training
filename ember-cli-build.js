9f41a28d222061b978854b76fc28bb69d1c6aa40
Fayne <fayne@mobilenowgroup.com>
2015-08-19 17:59:35 +0800

    add bootstrap into project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,18 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    vendorFiles: {
+      'bower_components/bootstrap/dist/js/bootstrap.min.js': false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
+  
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
